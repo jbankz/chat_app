27306b93eadb178c0f7b319fdf87fb6fdc7c71bd
Anonymous <vsquare396@gmail.com>
2019-03-11 09:06:30 -0700

    Initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:chat_app/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Chat Demo"),
+        ),
+        body: new ChatScreen());
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:chat_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Chat Demo",
+      home: new HomePage(),
+    );
+  }
+}
